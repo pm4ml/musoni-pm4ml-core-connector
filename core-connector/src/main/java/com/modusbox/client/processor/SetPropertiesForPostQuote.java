9f0f22c07d8da680a875160f8810eb4d464035e9
shun <shunleiwai.ko@thitsaworks.com>
2021-08-22 21:32:34 +0630

    MMD-1252 - Changes in Processor and mappings

--- core-connector/src/main/java/com/modusbox/client/processor/SetPropertiesForPostQuote.java
+++ core-connector/src/main/java/com/modusbox/client/processor/SetPropertiesForPostQuote.java
@@ -8,9 +8,8 @@ public class SetPropertiesForPostQuote implements Processor {
 
     @Override
     public void process(Exchange exchange) throws Exception {
-        System.out.println("**** From Processor***"+exchange.getIn().getBody());
-
-        QuoteRequest quoteRequest = exchange.getIn().getBody(QuoteRequest.class);
+        
+		QuoteRequest quoteRequest = exchange.getIn().getBody(QuoteRequest.class);
         String quoteId=  quoteRequest.getQuoteId();
         String transactionId= quoteRequest.getTransactionId();
         String amount = quoteRequest.getAmount();
