23f5d265409c6fb00ba13e35790fb368ad7a5149
shun <shunleiwai.ko@thitsaworks.com>
2021-10-15 19:29:21 +0630

    MMD-1716 + MMD-1717 Update CC for Disbursement (VisionFund)

--- core-connector/src/main/java/com/modusbox/client/router/SendmoneyRouter.java
+++ core-connector/src/main/java/com/modusbox/client/router/SendmoneyRouter.java
@@ -50,25 +50,29 @@ public class SendmoneyRouter extends RouteBuilder {
                 .removeHeaders("CamelHttp*")
                 .setHeader(Exchange.HTTP_METHOD, constant("POST"))
                 .setHeader("Content-Type", constant("application/json"))
+
                 // Prune empty items from the request
+                .marshal().json()
                 .transform(datasonnet("resource:classpath:mappings/postSendMoneyRequest.ds"))
                 .setBody(simple("${body.content}"))
                 .marshal().json()
+
                 .to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
                         "'Calling outbound API, postTransfers, " +
                         "POST {{ml-conn.outbound.host}}', " +
                         "'Tracking the request', 'Track the response', 'Input Payload: ${body}')")
-//            .marshal().json(JsonLibrary.Gson)
-//.process(exchange -> System.out.println())
+
                 .toD("{{ml-conn.outbound.host}}/transfers?bridgeEndpoint=true")
                 .unmarshal().json()
                 .to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
                         "'Response from outbound API, postTransfers: ${body}', " +
                         "'Tracking the response', 'Verify the response', null)")
-//.process(exchange -> System.out.println())
+
+                //.process(exchange -> System.out.println())
                 .setProperty("postSendMoneyInitial", body())
                 // Send request to accept the party instead of hard coding AUTO_ACCEPT_PARTY: true
                 .to("direct:putTransfersAcceptParty")
+                .to("direct:putTransfersAcceptQuote")
                 /*
                  * END processing
                  */
@@ -77,26 +81,49 @@ public class SendmoneyRouter extends RouteBuilder {
                 }).end()
         ;
 
-        from("direct:putTransfersAcceptParty")
-                .routeId("com.modusbox.putTransfersAcceptParty")
+        from("direct:putTransfersAcceptParty").routeId("com.modusbox.putTransfersAcceptParty")
                 .removeHeaders("CamelHttp*")
                 .setHeader(Exchange.HTTP_METHOD, constant("PUT"))
                 .setHeader("Content-Type", constant("application/json"))
-//.process(exchange -> System.out.println())
+
+                .marshal().json()
                 .transform(datasonnet("resource:classpath:mappings/putTransfersAcceptPartyRequest.ds"))
                 .setBody(simple("${body.content}"))
                 .marshal().json()
+
                 .to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
                         "'Calling outbound API, putTransfersAcceptParty, " +
                         "PUT {{ml-conn.outbound.host}}/transfers/${exchangeProperty.postSendMoneyInitial?.get('transferId')}', " +
                         "'Tracking the request', 'Track the response', 'Input Payload: ${body}')")
-//.process(exchange -> System.out.println())
-//                .marshal().json()
+
                 // Instead of having to do a DataSonnet transformation
                 .toD("{{ml-conn.outbound.host}}/transfers/${exchangeProperty.postSendMoneyInitial?.get('transferId')}?bridgeEndpoint=true")
                 .unmarshal().json()
                 .to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
                         "'Response from outbound API, putTransfersAcceptParty: ${body}', " +
+                        "'Tracking the response', 'Verify the response', null)")
+        ;
+
+        from("direct:putTransfersAcceptQuote").routeId("com.modusbox.putTransfersAcceptQuote")
+                .removeHeaders("CamelHttp*")
+                .setHeader(Exchange.HTTP_METHOD, constant("PUT"))
+                .setHeader("Content-Type", constant("application/json"))
+
+                .marshal().json()
+                .transform(datasonnet("resource:classpath:mappings/putTransfersAcceptQuoteRequest.ds"))
+                .setBody(simple("${body.content}"))
+                .marshal().json()
+
+                .to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
+                        "'Calling outbound API, putTransfersAcceptQuote, " +
+                        "PUT {{ml-conn.outbound.host}}/transfers/${exchangeProperty.postSendMoneyInitial?.get('transferId')}', " +
+                        "'Tracking the request', 'Track the response', 'Input Payload: ${body}')")
+
+                // Instead of having to do a DataSonnet transformation
+                .toD("{{ml-conn.outbound.host}}/transfers/${exchangeProperty.postSendMoneyInitial?.get('transferId')}?bridgeEndpoint=true")
+                .unmarshal().json()
+                .to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
+                        "'Response from outbound API, putTransfersAcceptQuote: ${body}', " +
                         "'Tracking the response', 'Verify the response', null)")
         ;
 
@@ -115,19 +142,19 @@ public class SendmoneyRouter extends RouteBuilder {
                 .removeHeaders("CamelHttp*")
                 .setHeader(Exchange.HTTP_METHOD, constant("PUT"))
                 .setHeader("Content-Type", constant("application/json"))
+
                 // Will convert to JSON and only take the accept quote section
-                .transform(datasonnet("resource:classpath:mappings/putTransfersAcceptQuoteRequest.ds"))
+                .marshal().json()
+                .transform(datasonnet("resource:classpath:mappings/putTransfersRequest.ds"))
                 .setBody(simple("${body.content}"))
                 .marshal().json()
                 .to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
                         "'Calling outbound API, putTransfersById', " +
                         "'Tracking the request', 'Track the response', " +
                         "'Request sent to PUT https://{{ml-conn.outbound.host}}/transfers/${header.transferId}')")
+
                 .process(exchange -> System.out.println())
-//                .marshal().json(JsonLibrary.Gson)
-//                .marshal().json()
                 .toD("{{ml-conn.outbound.host}}/transfers/${header.transferId}?bridgeEndpoint=true")
-//                .unmarshal().json(JsonLibrary.Gson)
                 .unmarshal().json()
                 .to("bean:customJsonMessage?method=logJsonMessage('info', ${header.X-CorrelationId}, " +
                         "'Response from outbound API, putTransfersById: ${body}', " +
